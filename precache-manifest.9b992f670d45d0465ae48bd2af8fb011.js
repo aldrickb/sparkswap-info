b0f2b153e69e15673a691f017e4400b038126b1e
Aldrick Bonaobra <git@aldrickb.com>
2021-04-10 20:41:55 +0800

    Updates

---
+++ precache-manifest.9b992f670d45d0465ae48bd2af8fb011.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c4af5ba568c05f6c25e020545b4ba5",
+    "url": "/sparkswap-info/index.html"
+  },
+  {
+    "revision": "b92deeb4a460dc3e4862",
+    "url": "/sparkswap-info/static/js/2.c8b9552f.chunk.js"
+  },
+  {
+    "revision": "1bcf502cdba6b5e524b6436cdf7a8c7a",
+    "url": "/sparkswap-info/static/js/2.c8b9552f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c3a6f52b8b2569d895b",
+    "url": "/sparkswap-info/static/js/main.923a4141.chunk.js"
+  },
+  {
+    "revision": "19cd11921d3fc7c1b066",
+    "url": "/sparkswap-info/static/js/runtime-main.24abc3db.js"
+  },
+  {
+    "revision": "06d5ee921a83a8619d38e63c3c032ca1",
+    "url": "/sparkswap-info/static/media/SparkDeFi_wordmark.06d5ee92.svg"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/sparkswap-info/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "63539be0688cab97a6a8dbd7915deaf0",
+    "url": "/sparkswap-info/static/media/logo_sqaure.63539be0.png"
+  }
+]);
